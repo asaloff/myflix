db0b3ff515dc648806cd71ed24bc33d2836c2002
Aaron Saloff <aaronsaloff@gmail.com>
2015-07-11 11:59:51 -0400

    finished precourse work

---
+++ db/migrate/20150709233247_change_description_to_text.rb
@@ -0,0 +1,8 @@
+class ChangeDescriptionToText < ActiveRecord::Migration
+  def up
+    change_column :videos, :description, :text
+  end
+  def down
+      change_column :videos, :description, :string
+  end
+end
