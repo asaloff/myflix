80ea2954f7fbc897cdbfd55979845c8b27b6ed34
Aaron Saloff <aaronsaloff@gmail.com>
2015-07-14 23:02:44 -0400

    wrote test for search_by_title and wired up search view

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,4 +2,10 @@ class Video < ActiveRecord::Base
   belongs_to :category
 
   validates_presence_of :title, :description
+  validates_uniqueness_of :title
+
+  def self.search_by_title(search_input)
+    return [] if search_input == ''
+    where("lower(title) LIKE ?", "%#{search_input.downcase}%").sort_by { |vid| vid.created_at }
+  end
 end
