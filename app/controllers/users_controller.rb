839d5d5ea3a43a341fd518aa63d625ea5e8e28b4
Aaron Saloff <aaronsaloff@gmail.com>
2015-07-18 12:56:32 -0400

    fixed btn-default class and made user_params method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params.require(:user).permit(:email, :password, :full_name))
+    @user = User.new(user_params)
 
     if @user.save
       flash['success'] = 'You have registered successfully'
@@ -12,5 +12,11 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :full_name)
   end
 end
