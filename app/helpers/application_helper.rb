a00a43ee1c53bdd40ad9929e94e978a55c4c65ce
Aaron Saloff <aaronsaloff@gmail.com>
2015-07-29 20:56:45 -0400

    started user can update my_queue rating

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def num_of_stars
-    [5,4,3,2,1].map { |num| [pluralize(num, 'Star'), num] }
+  def options_for_num_of_stars(selected=nil)
+    options_for_select([5,4,3,2,1].map { |num| [pluralize(num, 'Star'), num] }, selected)
   end
 end
