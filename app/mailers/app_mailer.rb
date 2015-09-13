d1e94d74abc54e3ce5b7c034aebb30bbf8cfc5a4
Aaron Saloff <aaronsaloff@gmail.com>
2015-09-13 16:49:31 -0400

    refactored staging conditional

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -1,28 +1,16 @@
 class AppMailer < ActionMailer::Base
   def send_welcome(user)
     @user = user
-    if Rails.env.staging?
-      mail from: 'info@myflix.com', to: ENV['TEST_EMAIL'], subject: 'Welcome to MyFlix!'
-    else
-      mail from: 'info@myflix.com', to: user.email, subject: 'Welcome to MyFlix!'
-    end
+    mail from: 'info@myflix.com', to: Rails.env.staging? ? ENV['TEST_EMAIL'] : user.email, subject: 'Welcome to MyFlix!'
   end
 
   def send_password_reset(user)
     @user = user
-    if Rails.env.staging?
-      mail from: 'info@myflix.com', to: ENV['TEST_EMAIL'], subject: 'Reset Your Myflix Password'
-    else
-    mail from: 'customersupport@myflix.com', to: user.email, subject: 'Reset your MyFlix Password'
-    end
+    mail from: 'info@myflix.com', to: Rails.env.staging? ? ENV['TEST_EMAIL'] : user.email, subject: 'Reset Your Myflix Password'
   end
 
   def send_friend_signup(invitation)
     @invitation = invitation
-    if Rails.env.staging?
-      mail from: 'info@myflix.com', to: ENV['TEST_EMAIL'], subject: "#{@invitation.inviter.full_name} wants you to join MyFlix..."
-    else
-      mail from: 'info@myflix.com', to: @invitation.invitee_email, subject: "#{@invitation.inviter.full_name} wants you to join MyFlix..."
-    end
+    mail from: 'info@myflix.com', to: Rails.env.staging? ? ENV['TEST_EMAIL'] : @invitation.invitee_email, subject: "#{@invitation.inviter.full_name} wants you to join MyFlix..."
   end
 end
