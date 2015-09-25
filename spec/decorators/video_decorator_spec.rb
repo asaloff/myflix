b06a417cffe9e348512060f2b97c68227e8edfdb
Aaron Saloff <aaronsaloff@gmail.com>
2015-09-25 06:51:19 -0400

    fixed method name in test for VideoDecorater

--- spec/decorators/video_decorator_spec.rb
+++ spec/decorators/video_decorator_spec.rb
@@ -6,11 +6,11 @@ describe VideoDecorator do
 
     it "returns the rating if the video has any reviews" do
       Fabricate(:review, video: video, rating: 5)
-      expect(video.rating).to eq "Average Rating: 5.0 / 5"
+      expect(video.display_rating).to eq "Average Rating: 5.0 / 5"
     end
 
     it "returns 'N/A if the video has no reviews" do
-      expect(video.rating).to eq "N/A"
+      expect(video.display_rating).to eq "N/A"
     end
   end
 end
